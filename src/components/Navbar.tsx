2570bef705ec964499cf949c814dd0bab409b6c3
mohorrominesa25-crypto <mohorrominesa25@gmail.com>
2026-09-13 19:53:25 +0600

    navbar responsive created

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,83 @@
-import Logo from "../assets/logo-text.png"
-const Navbar = () => {
-    return (
-        <nav className="sticky top-0 z-50 flex justify-between container mx-auto px-25 py-5 font-semibold text-[#475569]">
-            <div>
-          <img src={Logo} alt="" />  
-        </div>
-        
-            <ul className="flex justify-between gap-6  ">
-                <li className="text-pink-600"><a href="">Home</a></li>
-          <li className="hover:text-pink-600"><a href="">Technologies</a></li>
-          <li className="hover:text-pink-600"><a href="">Projects</a></li>
-          <li className="hover:text-pink-600"><a href="">About</a></li>
-          <li className="hover:text-pink-600"><a href="">Contact</a></li></ul>
-        
-        <div className="flex justify-between gap-5">
-            <button className="hover:scale-105 ">Sign In</button>
-          <button className=" hover:scale-105 bg-pink-600 px-3.5 rounded-[10px] text-white">Sign Up</button></div>
-        </nav>
+import { FaBars } from "react-icons/fa";
+import Logo from "../assets/logo-text.png";
 
-    );
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 z-50 container mx-auto px-5 md:px-25 py-5 font-semibold text-[#475569]">
+
+      
+      <div className="flex items-center justify-between md:hidden">
+        
+        
+        <button className="text-xl">
+          <FaBars />
+        </button>
+
+        
+        <div>
+          <img src={Logo} alt="Logo" className="w-24" />
+        </div>
+
+        
+        <div className="flex gap-2">
+          <button className="hover:scale-105">
+            Sign In
+          </button>
+
+          <button className="hover:scale-105 bg-pink-600 px-3 py-2 rounded-[10px] text-white">
+            Sign Up
+          </button>
+        </div>
+
+      </div>
+
+
+      
+      <div className="hidden md:flex items-center justify-between">
+
+        
+        <div>
+          <img src={Logo} alt="Logo" />
+        </div>
+
+        
+        <ul className="flex gap-6">
+          <li className="text-pink-600">
+            <a href="">Home</a>
+          </li>
+
+          <li className="hover:text-pink-600">
+            <a href="">Technologies</a>
+          </li>
+
+          <li className="hover:text-pink-600">
+            <a href="">Projects</a>
+          </li>
+
+          <li className="hover:text-pink-600">
+            <a href="">About</a>
+          </li>
+
+          <li className="hover:text-pink-600">
+            <a href="">Contact</a>
+          </li>
+        </ul>
+
+        
+        <div className="flex gap-5">
+          <button className="hover:scale-105">
+            Sign In
+          </button>
+
+          <button className="hover:scale-105 bg-pink-600 px-3.5 py-2 rounded-[10px] text-white">
+            Sign Up
+          </button>
+        </div>
+
+      </div>
+
+    </nav>
+  );
 };
 
 export default Navbar;
